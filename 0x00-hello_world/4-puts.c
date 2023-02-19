257fae05c7887118dfbe9e1d16f5172c23310655
Daramola Oluwatoyin <victoria.mnpadm@gmail.com>
2023-02-19 08:20:18 -0800

    C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,14 @@
 
 #include<stdio.h>
 
+/**
+ * main - Entry point
+ * Description: 'the use of puts()'
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-char str[] = "\"Programming is like building a multilingual puzzle\n";
+char str[] = "\"Programming is like building a multilingual puzzle";
 
 puts(str);
 
