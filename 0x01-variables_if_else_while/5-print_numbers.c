71d5c3e46c10cfaa5f3b221d7bda4ae1ce4940aa
Daramola Oluwatoyin <victoria.mnpadm@gmail.com>
2023-02-22 00:48:01 -0800

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ * the function should print numbers 0-9
+ * Return: 0; success
+ */
+
+int main(void)
+
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+		printf("%d", n);
+
+	return (0);
+}
+
