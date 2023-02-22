21fa50a683a2feb41009a1fabd78bdfa3f7e578e
Daramola Oluwatoyin <victoria.mnpadm@gmail.com>
2023-02-22 01:11:32 -0800

    numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * the fuction prints numbers
+ * Return: 0; success
+ */
+
+int main(void)
+{
+	int n = 0;
+
+	for (n = 0; n < 10; n++)
+		putchar(n);
+	putchar('\n');
+
+	return (0);
+
+}
