71e07607e9d075e352df09c6e34869df1d8ee66a
Daramola Oluwatoyin <victoria.mnpadm@gmail.com>
2023-02-24 00:31:03 -0800

    print putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * description - program prints a value
+ * Return: 0; (success)
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+
+}
