03aa5dd35e681b21cdea53ea4c553051bbdfd9af
Daramola Oluwatoyin <victoria.mnpadm@gmail.com>
2023-02-25 04:52:28 -0800

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -14,12 +14,12 @@ int print_last_digit(int i)
 	l = i % 10;
 	if (l < 0)
 	{
-		_putchar(-l + 0);
+		_putchar(-l + 48);
 		return (-l);
 	}
 	else
 	{
-		_putchar(l + 0);
+		_putchar(l + 48);
 		return (l);
 	}
 
