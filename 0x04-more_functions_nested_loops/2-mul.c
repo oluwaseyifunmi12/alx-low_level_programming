2fd1e700019dd174f1218a132c508f64705b5e95
Daramola Oluwatoyin <victoria.mnpadm@gmail.com>
2023-02-26 15:25:49 -0800

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul -[Entry point
+ * @a: - integers to be checked
+ * @b: - another integer to be checked
+ * Description: multiplies two values
+ * Return: (0);
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
