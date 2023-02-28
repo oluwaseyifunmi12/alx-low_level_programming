aaecd476e133731c582b28fa933e580e793ff9f9
Daramola Oluwatoyin <victoria.mnpadm@gmail.com>
2023-02-27 17:40:08 -0800

    pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Entry point
+ * @n: pointer to be checked
+ *
+ * Description - takes a pointer to int
+ * Return: 98;
+*/
+
+void reset_to_98(int *n)
+{
+
+	*n = 98;
+
+}
