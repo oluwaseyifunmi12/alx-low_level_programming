b944cf6681d235902ec3a0334cba0402f793197b
Daramola Oluwatoyin <victoria.mnpadm@gmail.com>
2023-03-21 13:24:00 -0700

    Structures

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef _DOG_H_
 #define _DOG_H_
 
+/**
+ * struct dog - structure for dog details
+ * @name: parameter 1
+ * @age: parameter 2
+ * @owner: parameter 3
+ */
+
 struct dog
 {
 	char *name;
