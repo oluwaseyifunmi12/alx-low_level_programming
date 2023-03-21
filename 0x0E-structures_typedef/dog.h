8a17047a9d05a3e1b718e1140ac4d08b62c84e1c
Daramola Oluwatoyin <victoria.mnpadm@gmail.com>
2023-03-21 15:43:29 -0700

    Structures

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,13 +8,13 @@
  * @owner: parameter 3
  */
 
-struct dog
+typedef struct dog 
 {
 	char *name;
 	float age;
 	char *owner;
 
-};
+}dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
