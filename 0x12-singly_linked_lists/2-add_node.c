0ccd63f4b436c7c983f0284c7f2e9508cac6e79e
Daramola Oluwatoyin <victoria.mnpadm@gmail.com>
2023-04-04 14:26:50 -0700

    insertion

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * add_node - adds a new node to a linked list
+ * @head: - the head node
+ * @str: - string to be printed out
+ * Return: address of the new node
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *newnode;
+
+	size_t size = 0;
+
+	newnode = malloc(sizeof(list_t));
+
+	if (newnode == NULL)
+		return (NULL);
+
+	newnode->str = strdup(str);
+
+	while (newnode->str != NULL)
+		size++;
+
+	newnode->len = size;
+
+	newnode->next = *head;
+
+	*head = newnode;
+
+	return (*head);
+}
+
