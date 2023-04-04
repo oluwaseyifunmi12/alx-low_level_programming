b2e12c5be96865f0fa399d7ccdbf8573e61c2081
Daramola Oluwatoyin <victoria.mnpadm@gmail.com>
2023-04-04 08:44:52 -0700

    second commit on linked list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,18 +12,18 @@ size_t print_list(const list_t *h)
 	size_t size;
 
 	size = 0;
-	
+
 	while (h != NULL)
 	{
-		if (h -> str == NULL)
+		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
-		
-			printf("%s\n", h -> str);
 
-		h = h -> next;
+			printf("[%d] %s\n", h->len, h->str);
+
+		h = h->next;
 		size++;
-		
+
 	}
 	return (size);
 }
