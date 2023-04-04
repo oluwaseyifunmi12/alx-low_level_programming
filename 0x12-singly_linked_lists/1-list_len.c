dd286ab2438341d63e6c17d37fbe11587091511f
Daramola Oluwatoyin <victoria.mnpadm@gmail.com>
2023-04-04 08:59:45 -0700

    second commit on linked list

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_list - function prints elements of list_t
+ * @h: singly linked list
+ * Return: the number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t size;
+
+	size = 0;
+
+	while (h != NULL)
+	{
+
+		h = h->next;
+		size++;
+
+	}
+	return (size);
+}
