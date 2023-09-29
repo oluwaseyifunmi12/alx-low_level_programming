7e632d0c71b7f85c9110d30e79ba44ac607be1c0
Daramola Oluwatoyin <victoria.mnpadm@gmail.com>
2023-09-28 19:07:22 -0700

    printing a string with recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,27 @@
+
+#include "main.h"
+
+/**
+ * _puts_recursion - Name of the function prototype
+ *
+ * @s: Character to be printed
+ *
+ * Return: 0
+ */
+
+void _puts_recursion(char *s)
+
+{
+	if (*s == '\0')
+
+	{
+		_putchar('\n');
+
+		return;
+	}
+
+	else
+		_putchar(*s);
+
+		_puts_recursion(s + 1);
+}
