c96d19ad3b2ab0aa2d3e63c8228b6a5a291a4630
Daramola Oluwatoyin <victoria.mnpadm@gmail.com>
2023-09-29 19:22:14 -0700

    Printing a reverse string

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,21 @@
+iinclude "main.h"
+
+/**
+ * _print_rev_recursion - a function prototype to print a string in reverse
+ *
+ * @s: character to be printed
+ *
+ * Return: Always (0);
+ */
+
+void _print_rev_recursion(char *s);
+
+{
+
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+
+		_putchar(*s);
+	}
+}
