a03aa2f77618078fb41b0a8ed0223fbc394d4c77
Daramola Oluwatoyin <victoria.mnpadm@gmail.com>
2023-10-01 15:16:50 -0700

    factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * factorial - This function returns the factorial of a given number
+ *
+ * @n: this the value to be estimated
+ *
+ * Return: the factorial of the number
+ */
+
+int factorial(n)
+{
+	if (n < 0)
+		return (-1);
+
+	else if (n == 0)
+		return (1);
+
+	else
+		n = (n *factorial(n - 1));
+	return (n);
+}
