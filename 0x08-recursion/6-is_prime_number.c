531b7c95229547be441863b1e61a466bd1450e0c
Daramola Oluwatoyin <victoria.mnpadm@gmail.com>
2023-10-01 19:01:49 -0700

    prime numbers

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -14,8 +14,11 @@ int is_prime_number(int n)
 {
 	int m = 2;
 
-	if (n == 1 || n == 0)
+	if ( n == 0)
 		return (-1);
+
+	if ( n == 1 || n == 2)
+		return (1);
 
 	else if (n % m == 0)
 		return (1);
