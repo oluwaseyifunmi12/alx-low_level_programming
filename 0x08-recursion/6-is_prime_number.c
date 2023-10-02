185f90818a1303f8d9f0436517b66313399e1892
Daramola Oluwatoyin <victoria.mnpadm@gmail.com>
2023-10-01 18:57:09 -0700

    prime numbers

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * is_prime_number - Function to check prime number
+ *
+ * @n: number to be checked
+ *
+ * Return: (0);
+ *
+ */
+
+
+int is_prime_number(int n)
+{
+	int m = 2;
+
+	if (n == 1 || n == 0)
+		return (-1);
+
+	else if (n % m == 0)
+		return (1);
+
+	else
+		m++;
+	return (is_prime_number(n));
+}
--- 0x08-recursion/main.c
+++ 0x08-recursion/main.c
@@ -1,12 +1,21 @@
 #include "main.h"
+#include <stdio.h>
 
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
 int main(void)
 {
-    _puts_recursion("Puts with recursion");
+    int r;
+
+    r = _sqrt_recursion(1);
+    printf("%d\n", r);
+    r = _sqrt_recursion(1024);
+    printf("%d\n", r);
+    r = _sqrt_recursion(16);
+    printf("%d\n", r);
+    r = _sqrt_recursion(17);
+    printf("%d\n", r);
+    r = _sqrt_recursion(25);
+    printf("%d\n", r);
+    r = _sqrt_recursion(-1);
+    printf("%d\n", r);
     return (0);
 }
