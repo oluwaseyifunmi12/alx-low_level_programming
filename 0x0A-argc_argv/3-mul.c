e023ec37c0a8fdadee1ac6088b31787e5b92f434
Daramola Oluwatoyin <victoria.mnpadm@gmail.com>
2023-10-03 08:34:49 -0700

    multiplying arguments

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - this function multiplies its arguments and send its output
+ *
+ * @argc: number of argument
+ *
+ * @argv: argument string
+ *
+ * Return: (1); failure
+*/
+
+int main(int argc, char *argv[])
+{
+	int mul;
+
+	if (argc == 3)
+	{
+
+		mul = atoi(argv[1]) * atoi(argv[2]);
+			printf("%d\n", mul);
+			return (0);
+	}
+
+	else
+		{
+                printf("Error\n");
+                return (1);
+        }
+
+}
