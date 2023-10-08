8011fcc3414cc9a5eaf42ad161f3b21dc457fbef
Daramola Oluwatoyin <victoria.mnpadm@gmail.com>
2023-10-08 16:47:11 -0700

    concat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -16,7 +16,7 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *ptr;
-	unsigned int i, j, k, l;
+	unsigned int i, j, k;
 
 	if (s1 == NULL)
 		s1 = "";
@@ -41,10 +41,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (k = 0; k <= i; k++)
 		ptr[k] = s1[k];
 
-	for (l = 0; l <= n; l++)
-		ptr[i + l] = s2[l];
+	for (k = 0; k <= n; k++)
+		ptr[i + k] = s2[k];
 
-	ptr[i + l] = '\0';
+	ptr[i + k] = '\0';
 
 	return (ptr);
 
