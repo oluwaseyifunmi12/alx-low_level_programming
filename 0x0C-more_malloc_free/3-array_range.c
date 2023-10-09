6168f72bad645ac5529b5ebd04e5a357c6c27814
Daramola Oluwatoyin <victoria.mnpadm@gmail.com>
2023-10-08 18:08:38 -0700

    array of integers with malloc

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -23,10 +23,8 @@ int *array_range(int min, int max)
 	if (ptr == NULL)
 		return (NULL);
 
-	for (i = 0; min <= max; i++)
-	{
-		min++;
+	for (i = 0; min <= max; i++, min++)
 		ptr[i] = min;
-	}
+
 	return (ptr);
 }
