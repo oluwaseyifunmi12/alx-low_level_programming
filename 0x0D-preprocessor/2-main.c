cbf691176d4320e34c30e3858894f424b44c90d6
Daramola Oluwatoyin <victoria.mnpadm@gmail.com>
2023-10-10 08:30:57 -0700

    preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Function that prints the name of its file
+ * 
+ * description : name of file
+ * 
+ * Return: (0); success
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
