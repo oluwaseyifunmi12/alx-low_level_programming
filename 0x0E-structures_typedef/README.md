fdf7b6e9777ca3e727e59d9974f8601d5cebb6f9
Daramola Oluwatoyin <victoria.mnpadm@gmail.com>
2023-10-10 18:15:54 -0700

    struct

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+working on structures and types
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - structure for dog details
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ *
+ * Description - this stores basic info about a dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
