d27c9ed2f6fe4fc977b7030880ab8e61646e03de
Daramola Oluwatoyin <victoria.mnpadm@gmail.com>
2023-10-10 21:00:24 -0700

    free

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+
+/**
+ * dog_t - a new dog details function
+ * @new_dog: tag
+ *
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ *
+ * Return: void;
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	new_dog->name = struct dog
+}
---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog -  a function that frees dogs.
+ * 
+ * @d: dog details
+ *
+ * Return: nothing
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d);
+	}
+}
