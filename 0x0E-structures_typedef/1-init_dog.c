413e5b02874d029fec1fdddd828adac5ed99bf27
Daramola Oluwatoyin <victoria.mnpadm@gmail.com>
2023-10-10 19:08:36 -0700

    structures

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+
+/**
+ * init_dog - dog details function
+ * @d: tag
+ *
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ *
+ * Return: void;
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	struct dog *d = {.name = name, .age = age, .owner = owner};
+}
