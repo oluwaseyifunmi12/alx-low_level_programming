05fa7b187cfee54c72d0abae4ae3d62e94ce4c5f
Daramola Oluwatoyin <victoria.mnpadm@gmail.com>
2023-10-12 02:25:00 -0700

    print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * print_name - prints a name
+ *
+ * @name: name of the person
+ * @f: function  pointer
+ *
+ * Return: Nothing.
+ */
+
+void print_name(char *name, void (*f)(char *))
+
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,1 @@
+this is a file on the workings of function pointers
