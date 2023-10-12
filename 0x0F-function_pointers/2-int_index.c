a95e720ef6505d9d12c69ab75aa69d552aab772e
Daramola Oluwatoyin <victoria.mnpadm@gmail.com>
2023-10-12 04:15:12 -0700

    search

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,28 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - this function searches through element in an array
+ * @array: the given array
+ * @size: size of array
+ * @cmp: function pointer
+ *
+ * Return: Nothing.
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+
+{
+	int t;
+
+	if (cmp && array)
+	{
+		if (size <= 0)
+			return (-1);
+
+		for (t = 0; t < size; t++)
+			cmp(array[t]);
+	}
+	else
+		return (-1);
+
+}
