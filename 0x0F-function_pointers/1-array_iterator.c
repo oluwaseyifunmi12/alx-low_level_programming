abab5cd391e1c5c830acb666000aa4c4157e7ae2
Daramola Oluwatoyin <victoria.mnpadm@gmail.com>
2023-10-12 03:09:09 -0700

    iterator

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - this function mones through each element in an array
+ * @array: the given array
+ * @size: size of array
+ * @action: function pointer
+ *
+ * Return: Nothing.
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int));
+
+{
+	unsigned int t;
+
+	if (action && array)
+	{
+		for (t = 0; t < size; t++)
+			action(array[i]);
+	}
+}
