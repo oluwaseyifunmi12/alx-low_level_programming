07ed1eee2a9881765884a1fede06f96e96908a5b
Daramola Oluwatoyin <victoria.mnpadm@gmail.com>
2023-10-13 07:07:21 -0700

    adjusted

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,73 @@
+#include "3-calc.h"
+
+
+/**
+ * op_add - adds integers
+ * @a: first argument
+ * @b: second argument
+ *
+ * Return: Add
+ */
+
+int op_add(int a, int b)
+{
+       return (a + b); 
+}
+
+
+
+/**
+ * op_sub - subs integers
+ * @a: first argument
+ * @b: second argument
+ *
+ * Return : sub
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+
+/**
+ * op_mul - multiplies integers
+ * @a: first argument
+ * @b: second argument
+ *
+ * Return : mul
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+
+
+/**
+ * op_div - divides integers
+ * @a: first argument
+ * @b: second argument
+ *
+ * Return : div
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+
+
+/**
+ * op_mod - mods integers
+ * @a: first argument
+ * @b: second argument
+ *
+ * Return : mod
+ */
+int op_mod(int a, int b)
+{
+    return (a % b);
+}
--- README.md
+++ README.md
@@ -3,3 +3,4 @@ working on structures and types
 =======
 Learning memory allocation.
 >>>>>>> 9b20242f38edfca8d3a728b912b9b4e321d7cd54
+Readme conflict merged
