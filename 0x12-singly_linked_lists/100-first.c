ce285020a888de338d3292b0a6e61477836f1578
Daramola Oluwatoyin <victoria.mnpadm@gmail.com>
2023-10-19 21:40:16 -0700

    advancedd

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,7 +5,7 @@
  * Return: NULL.
  */
 
-void before_main(void) __attribute__((constructor))
+void __attribute__((constructor)) before_main()
 {
 	printf("You're beat! and yet, you must allow");
 	printf(",\nI bore my house upon my back!\n");
