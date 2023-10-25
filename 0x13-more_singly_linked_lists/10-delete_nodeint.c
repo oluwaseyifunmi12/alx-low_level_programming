6f7c917685b0f95c83332cf8190ed93451624ec7
Daramola Oluwatoyin <victoria.mnpadm@gmail.com>
2023-10-25 08:06:15 -0700

    delete

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,19 +1,18 @@
-iinclude "lists.h"
+#include "lists.h"
 
 /**
- * insert_nodeint_at_index - inserts a new node at a specified position
+ * delete_nodeint_at_index - inserts a new node at a specified position
  * @head: head of the list.
- * @idx: index of where the new node is inserted
- * @n: integer element.
+ * @index: position
  * Return: the address of the new node or NULL.
  */
 
-int delete_nodeint_at_index(struct listint_t **head, unsigned int index)
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	listint_t *temp;
 	listint_t *current;
 	listint_t *prev;
-        unsigned int i = 0;
+	unsigned int i = 0;
 
 	if (head == NULL || *head == NULL)
 	{
@@ -36,7 +35,7 @@ int delete_nodeint_at_index(struct listint_t **head, unsigned int index)
 	{
 		prev = current;
 		current = current->next;
-		i++
+		i++;
 	}
 
 	if (current == NULL)
@@ -44,8 +43,8 @@ int delete_nodeint_at_index(struct listint_t **head, unsigned int index)
 		return (-1);
 	}
 
-        prev->next = current->next;
-        free(current);
+	prev->next = current->next;
+	free(current);
 
 	return (1);
 
