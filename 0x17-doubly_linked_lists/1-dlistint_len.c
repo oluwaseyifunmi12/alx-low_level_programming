14b120164ff6ab502ce44e3ce064245ec295d6f7
victoria Daramola <victoria.mnpadm@gmail.com>
2024-02-02 07:33:04 -0800

    number of nodes

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - gives the length of a node
+ *
+ * @h: the head node
+ * Return: the number of nodes
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	const dlistint_t *current = h;
+	int count = 0;
+
+	while (current != NULL)
+	{
+		current = current->next;
+		count++;
+
+		printf("\n");
+	}
+
+	return (count);
+}
